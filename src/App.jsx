663fc470313076c031317e4663e7b877e5adef55
Takumi <nase49946@gmail.com>
2021-08-07 14:25:15 +0900

    タスク追加機能実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
 import React, { useState } from 'react';
 
 export const App = () => {
-  const [imcompleteTodos, setimcompleteTodos] = useState([
+  const [todoText, setTodoText] = useState('');
+  const [incompleteTodos, setIncompleteTodos] = useState([
     'ああああ',
     'いいいい',
   ]);
-  const [completeTodos, setcompleteTodos] = useState(['うううう']);
+  const [completeTodos, setCompleteTodos] = useState(['うううう']);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  };
 
   return (
     <React.Fragment>
       <div className="input-area">
-        <input type="text" placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p>未完了のTODO</p>
         <ul>
-          {imcompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo) => {
             return (
               <li className="list-row" key={todo}>
                 <p>{todo}</p>

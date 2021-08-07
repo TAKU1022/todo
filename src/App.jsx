df7d1b48359c5f972f995ce2fe355de6cf9fb472
Takumi <nase49946@gmail.com>
2021-08-07 09:32:46 +0900

    useStateを使用したState変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 
 const App = () => {
-  const onClickButton = () => alert();
+  const [number, setNumber] = useState(0);
+  const countUp = () => setNumber(number + 1);
+
   return (
     <React.Fragment>
       <h1 style={{ color: 'red' }}>こんにちわ！</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={countUp}>ボタン</button>
+      <p>{number}</p>
     </React.Fragment>
   );
 };

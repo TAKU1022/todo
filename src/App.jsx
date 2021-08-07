f561219a2c2b423837c7f9cdc83d650ac0c3c9b5
Takumi <nase49946@gmail.com>
2021-08-07 10:39:07 +0900

    useEffectを使用したレンダリング対応

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
-import ColorfulMessage from './components/ColorfulMessage';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { ColorfulMessage } from './components/ColorfulMessage';
 
 const App = () => {
   const [number, setNumber] = useState(0);
+  const [faceShowFlag, setfaceShowFlag] = useState(false);
+
   const countUp = () => setNumber(number + 1);
+  const switchFaceShowFlag = () => setfaceShowFlag(!faceShowFlag);
+
+  useEffect(() => {
+    if (number > 0) {
+      if (number % 3 === 0) {
+        faceShowFlag || setfaceShowFlag(true);
+      } else {
+        faceShowFlag && setfaceShowFlag(false);
+      }
+    }
+  }, [number]);
 
   return (
     <React.Fragment>
@@ -11,7 +25,9 @@ const App = () => {
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です！</ColorfulMessage>
       <button onClick={countUp}>ボタン</button>
+      <button onClick={switchFaceShowFlag}>on/off</button>
       <p>{number}</p>
+      {faceShowFlag && <p>(　ﾟдﾟ)</p>}
     </React.Fragment>
   );
 };
